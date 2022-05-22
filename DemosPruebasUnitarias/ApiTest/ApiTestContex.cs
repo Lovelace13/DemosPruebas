9b1c2b4cb9c4b458ddecfe95e4d66afbc23d8b2a
Katiuska Marleny Marin Suarez <kmmarin@espol.edu.ec>
2022-05-22 11:54:13 -0500

    Folder ApiTest

---
+++ DemosPruebasUnitarias/ApiTest/ApiTestContex.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Api;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemosPruebasUnitarias
+{
+    public static class ApiTestContex
+    {
+        public static ApiAppContext GetApiAppContext()
+        {
+            var options = new DbContextOptionsBuilder<ApiAppContext>().  //Crea base en memoria
+                UseInMemoryDatabase(databaseName: "AppDb").Options;
+
+            var dbContext = new ApiAppContext(options);
+            //var dbContext_ = AddInitialData(dbContext);
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+
+            return dbContext;
+        }
+
+        // Se pone un AddInitialData por tabla Ej: AddInitialDataUser
+        public static ApiAppContext AddInitialData(ApiAppContext context)
+        {
+            context.Users.Add(new User() { 
+                Name = "User Test1", 
+                LastName = "User Test2" 
+            });
+
+            context.SaveChanges();
+
+            return context;
+        }
+    }
+}
---
+++ DemosPruebasUnitarias/ApiTest/UserControllerTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Api;
+
+namespace DemosPruebasUnitarias.ApiTest
+{
+    public class UserControllerTest
+    {
+        [Fact]
+        public void UserGet()
+        {
+            using var apiContext = ApiTestContex.GetApiAppContext();
+            var userController = new Api.Controllers.UserController(apiContext);
+
+            var result = userController.Get();
+
+            Assert.Equal(3, result.Count());
+
+        }
+
+        [Fact]
+        public void UserGetListActive()
+        {
+            using var apiContext = ApiTestContex.GetApiAppContext();
+            var userController = new Api.Controllers.UserController(apiContext);
+
+            var result = userController.Get().ToList()[0];
+            result.Active = false;
+
+            userController.Put(result.UserId.ToString(), result);
+
+            var listResult = userController.Get();
+
+            Assert.True(listResult.ToList()[0].Active);
+            Assert.Equal(2, listResult.Count());
+
+        }
+
+        [Fact]
+        public void UserPost()
+        {
+            using var apiContext = ApiTestContex.GetApiAppContext();
+            var userController = new Api.Controllers.UserController(apiContext);
+
+            User newUser = new User()
+            {
+                Name = "User Test",
+                LastName = "LastName test"
+            };
+
+            userController.Post(newUser).Wait();
+            var result = userController.Get();
+
+            Assert.Equal(4, result.Count());
+            Assert.Equal("User Test", result.ToList()[3].Name);
+        }
+
+        [Fact]
+        public void UserPut()
+        {
+            using var apiContext = ApiTestContex.GetApiAppContext();
+            var userController = new Api.Controllers.UserController(apiContext);
+
+            var result = userController.Get().ToList()[0];
+            result.Name = "User Updated";
+
+            userController.Put(result.UserId.ToString(), result);
+            var listResult = userController.Get().ToList();
+
+            Assert.Equal(3, listResult.Count());
+            Assert.Equal("User Updated", listResult.ToList()[0].Name);
+        }
+
+        [Fact]
+        public void UserDelete()
+        {
+            using var apiContext = ApiTestContex.GetApiAppContext();
+            var userController = new Api.Controllers.UserController(apiContext);
+
+            var firstElement = userController.Get().ToList()[0];
+            var result = userController.Delete(firstElement.UserId.ToString());
+            var userlist = userController.Get();
+
+            Assert.Equal(2, userlist.Count());
+        }
+    }
+}
