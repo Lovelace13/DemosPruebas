7b46a031cba60ff4b24faf0cf49a16dc1ce0b869
Katiuska Marleny Marin Suarez <kmmarin@espol.edu.ec>
2022-05-28 23:24:48 -0500

    Reporte con coverage collector

--- ApiTest/Context/ApiAppContext.cs
+++ ApiTest/Context/ApiAppContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace Api
 {
     public class ApiAppContext : DbContext
--- ApiTest/Controllers/UserController.cs
+++ ApiTest/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Api.Controllers
 {
